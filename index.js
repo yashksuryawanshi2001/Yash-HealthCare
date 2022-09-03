2fbc3b82564dd85f25adc205d11aa867ffa3353f
yashksuryawanshi2001 <90770463+yashksuryawanshi2001@users.noreply.github.com>
2022-09-03 23:59:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+});
